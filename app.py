69e3ed01ffe6b240c76d49cb21983620e4f8b795
Amol <amol.k.bhatia@gmail.com>
2021-04-04 21:47:30 -0400

    update to App endings

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/tempdate<start><br/>"
-        f"/api/v1.0/tempdaterange/<start>/<end><br/>"  
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"  
         )
 
 @app.route("/api/v1.0/stations")
